53be6ace52a15cebfeff49946ae330c704e38559
Ben Nadel <ben@bennadel.com>
2024-09-26 10:05:09 -0400

    Add skip-to-main link.

--- app/content/playground/layouts/default/default.view.js
+++ app/content/playground/layouts/default/default.view.js
@@ -3,3 +3,28 @@
 
 // Import module styles.
 import "./default.view.less";
+
+// ----------------------------------------------------------------------------------- //
+// ----------------------------------------------------------------------------------- //
+
+window.mae7a24 = {
+	AppShell
+};
+
+function AppShell() {
+
+	return {
+		focusMain: focusMain
+	};
+
+	/**
+	* I programmatically focus the main content area.
+	*/
+	function focusMain() {
+
+		this.$refs.main.focus();
+		this.$refs.main.scrollIntoView();
+
+	}
+
+}
